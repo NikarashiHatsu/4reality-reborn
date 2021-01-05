e77f98ee602cf3d4710bcad284a4ea058a6bc939
NikarashiHatsu <yourlovelydev@gmail.com>
2021-01-06 01:42:56 +0700

    ui: responsive for social media section

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="flex flex-col items-center justify-center py-16 bg-gray-700">
-        <h3 class="text-4xl text-center sans-serif text-white font-semibold mb-8">
+        <h3 class="text-2xl sm:text-4xl text-center sans-serif text-white font-semibold mb-8">
             Our Social Media
         </h3>
         <div class="flex">
-            <a href="https://web.facebook.com/4rvtuberteam" target="_blank" class="flex flex-col mr-20">
-                <i class="fab fa-facebook-f text-white fa-3x"></i>
+            <a href="https://web.facebook.com/4rvtuberteam" target="_blank" class="flex flex-col mr-10 sm:mr-20">
+                <i class="fab fa-facebook-f text-white text-4xl sm:text-6xl"></i>
             </a>
-            <a href="https://twitter.com/4RealityTeam" target="_blank" class="flex flex-col mr-20">
-                <i class="fab fa-twitter text-white fa-3x"></i>
+            <a href="https://twitter.com/4RealityTeam" target="_blank" class="flex flex-col mr-10 sm:mr-20">
+                <i class="fab fa-twitter text-white text-4xl sm:text-6xl"></i>
             </a>
-            <a href="https://discord.link/4Reality" target="_blank" class="flex flex-col mr-20">
-                <i class="fab fa-discord text-white fa-3x"></i>
+            <a href="https://discord.link/4Reality" target="_blank" class="flex flex-col mr-10 sm:mr-20">
+                <i class="fab fa-discord text-white text-4xl sm:text-6xl"></i>
             </a>
             <a href="https://www.youtube.com/channel/UCfSxEUm5k2OHufxOAEul9OQ" target="_blank" class="flex flex-col">
-                <i class="fab fa-youtube text-white fa-3x"></i>
+                <i class="fab fa-youtube text-white text-4xl sm:text-6xl"></i>
             </a>
         </div>
     </div>
