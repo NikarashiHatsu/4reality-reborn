670a99117c084787e5e230eee06848ae0344d84f
NikarashiHatsu <yourlovelydev@gmail.com>
2021-01-18 13:58:21 +0700

    feat: added smoothscroll

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import SmoothScroll from 'smooth-scroll'
+
+let scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500
+})
 
 createApp(App).mount('#app')
